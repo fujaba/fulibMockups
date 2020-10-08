81871cc908eabb93d23924a9b9f9383ff11088d5
Adrian Kunz <clashsoft@hotmail.com>
2020-10-08 11:44:33 +0200

    fix: Clean up MockupTools

--- src/main/java/org/fulib/mockups/MockupTools.java
+++ src/main/java/org/fulib/mockups/MockupTools.java
@@ -5,63 +5,69 @@ import com.codeborne.selenide.Selenide;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class MockupTools
 {
-   public static MockupTools htmlTool()
-   {
-      return new MockupTools();
-   }
+	public static MockupTools htmlTool()
+	{
+		return new MockupTools();
+	}
 
-   public void dump(String file, Object page)
-   {
-      LinkedHashMap<String, String> params = new LinkedHashMap<>();
+	public void dump(String file, Object page)
+	{
+		final Map<String, String> params = new LinkedHashMap<>();
 
-      Class<?> clazz = page.getClass();
-      try {
-         Method getParameters = clazz.getMethod("getParameters");
-         Collection paramSet = (Collection) getParameters.invoke(page);
-         if (paramSet != null) {
-            for (Object param : paramSet) {
-               clazz = param.getClass();
-               Method getKey = clazz.getMethod("getKey");
-               String key = (String) getKey.invoke(param);
-               Method getValue = clazz.getMethod("getValue");
-               String value = (String) getValue.invoke(param);
-               params.put(key, value);
-            }
-         }
-      }
-      catch (Exception e) {
-         e.printStackTrace();
-      }
+		try
+		{
+			final Method getParameters = page.getClass().getMethod("getParameters");
+			final Collection<?> paramSet = (Collection<?>) getParameters.invoke(page);
+			if (paramSet != null)
+			{
+				for (Object param : paramSet)
+				{
+					final Class<?> clazz = param.getClass();
+					final String key = (String) clazz.getMethod("getKey").invoke(param);
+					final String value = (String) clazz.getMethod("getValue").invoke(param);
+					params.put(key, value);
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 
+		final String html = FulibHtml.get(page, params);
 
-      String html = FulibHtml.get(page, params);
+		final Path path = Paths.get(file);
+		try
+		{
+			Files.createDirectories(path.getParent());
+			Files.write(path, html.getBytes());
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 
-      Path path = Paths.get(file);
-      Path absolutePath = path.toAbsolutePath();
-      Path dir = path.getParent();
-      try {
-         Files.createDirectories(dir);
-         Files.write(path, html.getBytes());
-      }
-      catch (IOException e) {
-         e.printStackTrace();
-      }
-
-      // produce svg, too
-      Configuration.browserSize = "400x600";
-      Configuration.reportsFolder = ".";
-      Selenide.open("file:///" + absolutePath.toString());
-      String result = Selenide.screenshot(file);
-
-      System.out.println();
-
-   }
+		// TODO produce svg, too
+		Configuration.browserSize = "400x600";
+		Configuration.reportsFolder = ".";
+		try
+		{
+			Selenide.open(path.toUri().toURL());
+		}
+		catch (MalformedURLException e)
+		{
+			e.printStackTrace();
+		}
+		Selenide.screenshot(file);
+	}
 }
