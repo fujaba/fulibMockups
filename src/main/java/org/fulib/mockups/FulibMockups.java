b9536f5e0d594d2a6e66b3485af6d2741e7f18b5
Adrian Kunz <clashsoft@hotmail.com>
2020-10-08 12:43:49 +0200

    feat: Add FulibMockups.htmlTool

--- src/main/java/org/fulib/mockups/FulibMockups.java
+++ src/main/java/org/fulib/mockups/FulibMockups.java
@@ -1,9 +1,16 @@
 package org.fulib.mockups;
 
+import org.fulib.scenarios.MockupTools;
+
 public class FulibMockups
 {
 	public static MockupTool mockupTool()
 	{
 		return new MockupTool();
+	}
+
+	public static MockupTools htmlTool()
+	{
+		return new MockupTools();
 	}
 }
