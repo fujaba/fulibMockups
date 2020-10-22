d40fb40be3377bc2c1f26fc058cc6f9926418efa
Adrian Kunz <clashsoft@hotmail.com>
2020-10-22 14:59:59 +0200

    feat: Produce .html and .png files independently
    Also prevents creation of .html.png.html files and reports.

--- src/main/java/org/fulib/mockups/MockupTool.java
+++ src/main/java/org/fulib/mockups/MockupTool.java
@@ -4,9 +4,10 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.fulib.mockups.model.Parameter;
 import org.fulib.mockups.model.Ui;
+import org.openqa.selenium.OutputType;
 
+import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,6 +18,29 @@ public class MockupTool
 {
 	public void dump(String file, Ui page)
 	{
+		final String htmlFile;
+		final String pngFile;
+		if (file.endsWith(".html.png"))
+		{
+			htmlFile = file.substring(0, file.length() - ".png".length());
+			pngFile = file;
+		}
+		else if (file.endsWith(".png"))
+		{
+			htmlFile = null;
+			pngFile = file;
+		}
+		else if (file.endsWith(".html"))
+		{
+			htmlFile = file;
+			pngFile = null;
+		}
+		else
+		{
+			throw new IllegalArgumentException(
+				"invalid file name '" + file + "' - extension must be .html.png, .png or .html");
+		}
+
 		final Map<String, String> params = new LinkedHashMap<>();
 		for (Parameter param : page.getParameters())
 		{
@@ -25,7 +49,20 @@ public class MockupTool
 
 		final String html = UiRenderer.render(page, params);
 
-		final Path path = Paths.get(file);
+		if (htmlFile != null)
+		{
+			writeHtmlFile(htmlFile, html);
+		}
+
+		if (pngFile != null)
+		{
+			writePngFile(pngFile, html);
+		}
+	}
+
+	private static void writeHtmlFile(String htmlFile, String html)
+	{
+		final Path path = Paths.get(htmlFile);
 		try
 		{
 			Files.createDirectories(path.getParent());
@@ -35,19 +72,24 @@ public class MockupTool
 		{
 			e.printStackTrace();
 		}
+	}
 
+	private static void writePngFile(String pngFile, String html)
+	{
 		// TODO produce svg, too
 		Configuration.browserSize = "400x600";
 		Configuration.headless = true;
-		Configuration.reportsFolder = ".";
-		try
-		{
-			Selenide.open(path.toUri().toURL());
-		}
-		catch (MalformedURLException e)
-		{
-			e.printStackTrace();
-		}
-		Selenide.screenshot(file);
+		Configuration.savePageSource = false;
+
+		final String dataUrl = "data:text/html," + html;
+
+		Selenide.open(dataUrl);
+
+		final File screenshotFile = Selenide.screenshot(OutputType.FILE);
+		final File dest = new File(pngFile);
+
+		dest.delete();
+		dest.getParentFile().mkdirs();
+		screenshotFile.renameTo(dest);
 	}
 }
