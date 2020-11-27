0822db03e470549fe8845c508b43b23de4191034
Adrian Kunz <clashsoft@hotmail.com>
2020-11-27 18:06:43 +0100

    refactor: Helper methods for rendering description items

--- src/main/java/org/fulib/mockups/UIRenderer.java
+++ src/main/java/org/fulib/mockups/UIRenderer.java
@@ -3,11 +3,31 @@ package org.fulib.mockups;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroupFile;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class UIRenderer
 {
 	private static final STGroupFile HTML_TEMPLATES = new STGroupFile(UIRenderer.class.getResource("html.stg"));
+
+	private static final Map<String, Renderer> RENDERERS;
+
+	interface Renderer
+	{
+		String render(String[] args, Map<String, String> params);
+	}
+
+	static
+	{
+		Map<String, Renderer> renderers = new HashMap<>();
+		renderers.put("button", UIRenderer::renderButton);
+		renderers.put("hidden", UIRenderer::renderHidden);
+		renderers.put("input", UIRenderer::renderInput);
+		renderers.put("cell", UIRenderer::renderCell);
+		renderers.put("---", UIRenderer::renderLine);
+		RENDERERS = Collections.unmodifiableMap(renderers);
+	}
 
 	private Map<String, String> params;
 
@@ -66,61 +86,16 @@ public class UIRenderer
 		final StringBuilder text = new StringBuilder();
 		for (String elem : description.split("\\|"))
 		{
-			elem = elem.trim();
-			if (elem.startsWith("button"))
+			final String[] args = elem.trim().split(" ");
+			if (args.length == 0)
 			{
-				String[] words = elem.split(" ");
-				String buttonText = words[1];
-				String target = buttonText;
-				if (words.length > 2)
-				{
-					target = words[2];
-				}
-				// button(text, target)
-				ST st = HTML_TEMPLATES.getInstanceOf("button");
-				st.add("text", buttonText);
-				st.add("target", target);
-				text.append(st.render());
+				continue;
 			}
-			else if (elem.startsWith("hidden"))
+
+			final Renderer renderer = RENDERERS.get(args[0]);
+			if (renderer != null)
 			{
-				String[] tokens = elem.split(" ");
-				String value = params.get(tokens[1]);
-				if (tokens.length > 2)
-				{
-					value = tokens[2];
-				}
-				// hidden(name, value)
-				ST st = HTML_TEMPLATES.getInstanceOf("hidden");
-				st.add("name", tokens[1]);
-				st.add("value", value);
-				text.append(st.render());
-			}
-			else if (elem.startsWith("input"))
-			{
-				String[] tokens = elem.split(" ");
-				String inputName = tokens[1];
-				int pos = elem.indexOf(inputName);
-				String prompt = elem.substring(pos + inputName.length()).trim();
-				String value = params.get(inputName);
-				// input(id, prompt, value)
-				ST st = HTML_TEMPLATES.getInstanceOf("input");
-				st.add("id", inputName);
-				st.add("prompt", prompt);
-				st.add("value", value);
-				text.append(st.render());
-			}
-			else if (elem.startsWith("cell"))
-			{
-				String[] tokens = elem.split(" ");
-				// cell(text)
-				ST st = HTML_TEMPLATES.getInstanceOf("cell");
-				st.add("text", tokens[1]);
-				text.append(st.render());
-			}
-			else if (elem.startsWith("---"))
-			{
-				text.append("<hr>\n");
+				text.append(renderer.render(args, this.params));
 			}
 			else
 			{
@@ -128,5 +103,54 @@ public class UIRenderer
 			}
 		}
 		return text.toString();
+	}
+
+	private static String renderButton(String[] args, Map<String, String> params)
+	{
+		String buttonText = args[1];
+		String target = args.length > 2 ? args[2] : buttonText;
+		// button(text, target)
+		ST st = HTML_TEMPLATES.getInstanceOf("button");
+		st.add("text", buttonText);
+		st.add("target", target);
+		return st.render();
+	}
+
+	private static String renderHidden(String[] args, Map<String, String> params)
+	{
+		final String name = args[1];
+		final String value = args.length > 2 ? args[2] : params.get(name);
+
+		// hidden(name, value)
+		ST st = HTML_TEMPLATES.getInstanceOf("hidden");
+		st.add("name", name);
+		st.add("value", value);
+		return st.render();
+	}
+
+	private static String renderInput(String[] args, Map<String, String> params)
+	{
+		String inputName = args[1];
+		String prompt = args[2];
+		String value = params.get(inputName);
+		// input(id, prompt, value)
+		ST st = HTML_TEMPLATES.getInstanceOf("input");
+		st.add("id", inputName);
+		st.add("prompt", prompt);
+		st.add("value", value);
+		return st.render();
+	}
+
+	private static String renderCell(String[] args, Map<String, String> params)
+	{
+		// cell(text)
+		ST st = HTML_TEMPLATES.getInstanceOf("cell");
+		st.add("text", args[1]);
+		return st.render();
+	}
+
+	private static String renderLine(String[] args, Map<String, String> params)
+	{
+		return "<hr/>\n";
 	}
 }
